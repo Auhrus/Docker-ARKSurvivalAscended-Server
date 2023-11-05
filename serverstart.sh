c14bf8ce68901133fdf19ee7a13a91ee7592d4b2
Auhrus <57270834+Auhrus@users.noreply.github.com>
2023-11-05 14:52:51 +0100

    serverstart.sh update Proton 8.22
    Updated Version of Proton 8.21 to 8.22

--- serverstart.sh
+++ serverstart.sh
@@ -9,5 +9,5 @@ fi
     echo "***Starting ARK Ascended Server"
 	export XDG_RUNTIME_DIR=/tmp/XDG_Runtime
 	tail -F "/home/steam/Steam/steamapps/common/ARK Survival Ascended Dedicated Server/ShooterGame/Saved/Logs/ShooterGame.log" &
-    STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/steam/Steam/" STEAM_COMPAT_DATA_PATH="/home/steam/Steam/steamapps/compatdata/2430930" "/home/steam/Steam/compatibilitytools.d/GE-Proton8-21/proton" run "/home/steam/Steam/steamapps/common/ARK Survival Ascended Dedicated Server\ShooterGame\Binaries\Win64\ArkAscendedServer.exe" $startcommands
+    STEAM_COMPAT_CLIENT_INSTALL_PATH="/home/steam/Steam/" STEAM_COMPAT_DATA_PATH="/home/steam/Steam/steamapps/compatdata/2430930" "/home/steam/Steam/compatibilitytools.d/GE-Proton8-22/proton" run "/home/steam/Steam/steamapps/common/ARK Survival Ascended Dedicated Server\ShooterGame\Binaries\Win64\ArkAscendedServer.exe" $startcommands
 exit
